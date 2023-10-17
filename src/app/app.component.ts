1c7f68d38b0da891d45a5ad73a8b8c5b4ca0553d
claudia.quezada <claudia.quezada@tisal.cl>
2023-10-17 12:01:17 -0300

    add()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'contador';
+  title: string = 'Contador';
+  counter: number = 0;
+
+  add() {
+    this.counter++;
+  }
 }
