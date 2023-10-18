ae3337c342c3f657ce4e9b9f04c2726ea103c421
claudia.quezada <claudia.quezada@tisal.cl>
2023-10-18 12:32:27 -0300

    integration testing

--- src/app/counter/button-sub/button-sub.component.spec.ts
+++ src/app/counter/button-sub/button-sub.component.spec.ts
@@ -18,4 +18,17 @@ describe('ButtonSubComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Debe inicializar el contador en cero. (Al inicializarse de forma independiente del counter)', () => { 
+    expect(component.counter).toBe(0);
+  });
+
+  it('Al ejecutar sub(), el valor del contador es -1.', () => { 
+    let newCounter = 0;
+    component.onSub.subscribe( contador => { // me suscribo al evento
+      newCounter = contador;
+    });
+    component.sub();
+    expect(newCounter).toBe(-1);
+  });
 });
--- src/app/counter/counter.component.spec.ts
+++ src/app/counter/counter.component.spec.ts
@@ -1,15 +1,19 @@
 import { CounterComponent } from "./counter.component"
 import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { ButtonAddComponent } from "./button-add/button-add.component";
+import { ButtonSubComponent } from "./button-sub/button-sub.component";
 
-describe('CounterComponent', ()=> {
+describe('CounterComponent Unit Testing', ()=> {
     let component: CounterComponent;
     let fixture: ComponentFixture<CounterComponent>;
 
     // Renderizar componente
     beforeEach(() => {
         TestBed.configureTestingModule({
-          declarations: [CounterComponent],
+            declarations: [
+                CounterComponent
+        ],
           schemas: [NO_ERRORS_SCHEMA]
         });
         fixture = TestBed.createComponent(CounterComponent);
@@ -17,7 +21,7 @@ describe('CounterComponent', ()=> {
         fixture.detectChanges();
     });
 
-    it('Se debe crear CounterComponent', ()=> {
+    it('Se debe crear CounterComponent', ()=> {      
         expect(component).toBeTruthy();
     });
 
@@ -31,4 +35,42 @@ describe('CounterComponent', ()=> {
         const counter = new CounterComponent();
         expect(counter.counter).toBe(23);
     });
-})
+});
+
+describe('CounterComponent Integration Testing', ()=> {
+    let component: CounterComponent;
+    let fixture: ComponentFixture<CounterComponent>;
+
+    // Renderizar componente
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                CounterComponent,
+                ButtonAddComponent,
+                ButtonSubComponent
+        ],
+          schemas: [NO_ERRORS_SCHEMA]
+        });
+        fixture = TestBed.createComponent(CounterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('Evento Click BtnAdd', ()=> {
+        const compiled = fixture.nativeElement as HTMLElement;
+        const counterValue = compiled.querySelector("h1");
+        const btnAdd: HTMLElement = fixture.debugElement.nativeElement.querySelector('#addBtn');
+        btnAdd.click();
+        fixture.detectChanges();
+        expect(counterValue?.textContent).toContain('Contador: 24');
+    });
+
+    it('Evento Click BtnSub', ()=> {
+        const compiled = fixture.nativeElement as HTMLElement;
+        const counterValue = compiled.querySelector("h1");
+        const btnSub: HTMLElement = fixture.debugElement.nativeElement.querySelector('#subBtn');
+        btnSub.click();
+        fixture.detectChanges();
+        expect(counterValue?.textContent).toContain('Contador: 22');
+    });
+});
